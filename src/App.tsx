469d56a243d04bac55b76119d89519a1e7f0b40d
imai-susumu <imai.susumu@jvckenwood.com>
2024-05-22 15:40:26 +0900

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+/* ima - login/out */
+import { Authenticator } from '@aws-amplify/ui-react';  // 認証用のAmplify UI Component
+import '@aws-amplify/ui-react/styles.css'
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -16,24 +20,41 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  /* ima - delete functions */
+  function deleteTodo(id:string) {
+    client.models.Todo.delete({ id });
+  }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    /* ima - login/out */
+    <Authenticator>
+      { /* ima - login/out */}
+      {({ signOut, user }) => (
+        <main>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li
+                onClick={() => deleteTodo(todo.id) } /* ima - delete function */
+                key={todo.id}>{todo.content}
+              </li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+          { /* ima - login/out */ } 
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      /* ima - login/out*/
+      )}
+    { /* ima - login/out */ }
+    </Authenticator>
   );
 }
 
